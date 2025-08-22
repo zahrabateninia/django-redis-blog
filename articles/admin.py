86847a9fc7b8d428e89058e5a0a0b1c36bc52b6d
zahrabateninia <zahrabatenin@gmail.com>
2025-08-22 20:01:25 +0330

    Create Article model

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "published_at", "updated_at")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "body")
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    body = models.TextField()
+    published_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-published_at"]
+
+    def __str__(self):
+        return self.title
